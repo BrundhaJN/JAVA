419bf5ca5d670f18a0672282e831cd1002659228
BrundhaJN <brundhajn.xworkz@gmail.comgit config --global user.email brundhajn.xworkz@gmail.com>
2021-09-07 23:40:33 +0530

    added file

--- Engineer.java
+++
@@ -1,9 +0,0 @@
-class Engineer{
-
-public static void main(String a[]){
-
-String nameOfEngineerBraches[]={"ECE","CSE","ISE","EEE","ME"};
-
-System.out.println(nameOfEngineerBraches[0]+" "+nameOfEngineerBraches[1]+" "+nameOfEngineerBraches[2]+" "+nameOfEngineerBraches[3]+" "+nameOfEngineerBraches[4]);
-}
-}
--- ShoppingMalls.java
+++
@@ -1,9 +0,0 @@
-class ShoppingMalls{
-
-public static void main(String a[]){
-
-String  namesofShoppingMalls[]={"Mantri Mall","Gardua Mall","Orion Mall"};
-
-System.out.println(namesofShoppingMalls[0]+" "+namesofShoppingMalls[1]+" "+namesofShoppingMalls[2]);
-}
-}
