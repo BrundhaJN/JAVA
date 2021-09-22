3e80257b938506b1e61e9eddb5734fe63fc91b34
BrundhaJN <brundhajn.xworkz@gmail.comgit config --global user.email brundhajn.xworkz@gmail.com>
2021-09-22 19:16:54 +0530

    added

---
+++ Laptop.java
@@ -0,0 +1,17 @@
+class Laptop{
+
+  public String name;
+  public int cost;
+  public String processor;
+  public int  memory;
+  public int  harddisk;
+  public  String operatingSystem;
+  
+  public void work(){
+  
+System.out.println("Latest version");
+
+  }
+  
+
+}
