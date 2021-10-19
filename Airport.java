386e4ae54abb71d093ced54437da88383d1a83da
BrundhaJN <brundhajn.xworkz@gmail.comgit config --global user.email brundhajn.xworkz@gmail.com>
2021-10-19 22:28:01 +0530

    Task

---
+++ Airport.java
@@ -0,0 +1,30 @@
+class Airport{
+
+public int airportId;
+public String airportName;
+public String location;
+public Terminal terminals;
+
+public Airport(){
+	
+	System.out.println(this.getClass().getSimpleName()+"object is created");
+}
+
+public void setAirportId(int airportId){
+	this.airportId=airportId;
+}
+public void setAirportName(String airportName){
+	this.airportName=airportName;
+}
+public void setLocation(String location){
+	this.location=location;
+}
+public void setTerminals(Terminal terminals){
+	this.terminals=terminals;
+}
+@Override
+public String toString(){
+	return "Airport-[airportId="+this.airportId+",airportName="+this.airportName+",location="+this.location+",terminals="+this.terminals+"]";
+}
+
+}
